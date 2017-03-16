86ab5f46b6127a321c15ac5edea26ef444e6f3ec
unknown <yongfeng9602@163.com>
2017-03-16 21:49:37 +0800

    [Python] add send email script

--- send_email.py
+++ send_email.py
@@ -2,15 +2,15 @@
 
 import smtplib
 
-USERNAME = 'xuyongfeng'
+USERNAME = 'zhangsan'
 
 def send_email(project,ret):
     smtp = smtplib.SMTP()
     smtp.connect("192.168.0.126", "25")   
-    smtp.login('xuyongfeng@droi.com', 'jake9602')   
-    smtp.sendmail('xuyongfeng@droi.com', USERNAME+'@droi.com', 
-    'From: '+'Compiler Server'+'\r\nTo: '+USERNAME+'@droi.com'+'\r\nSubject: Compile '+ret+'\r\n\r\n'+project)  
+    smtp.login('zhangsan@xxx.com', '123456')   
+    smtp.sendmail('zhangsan@xxxx.com', USERNAME+'@xxx.com', 
+    'From: '+'Compiler Server'+'\r\nTo: '+USERNAME+'@xxx.com'+'\r\nSubject: Compile '+ret+'\r\n\r\n'+project)  
     smtp.quit()   
 
 if __name__=="__main__":
-	send_email('Project_K506', 'Success');
+	send_email('Project_XXX', 'Success');
